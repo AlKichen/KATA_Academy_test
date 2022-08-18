274d5b7c4d5c659a22432a506b84b82d8ab03eea
AlKichen <kich7777777@gmail.com>
2022-08-18 16:13:45 +0300

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Calculator {
+    public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
